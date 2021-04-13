3fbe61960c6d3f39bc181cd0f5fb7f92a6f60e8f
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-04-13 00:56:46 -0600

    MP4 files support

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif|mp3|svg|eot|woff|woff2|ttf)$/i,
+        test: /\.(png|jpe?g|gif|mp3|svg|eot|woff|woff2|ttf|mp4)$/i,
         use: [
           {
             loader: "file-loader",

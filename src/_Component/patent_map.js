cccc97f240f72362c86c5375abd1772bbc8b8080
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-04-16 04:04:53 -0600

    PatentMap animation added and style changed

--- src/_Component/patent_map.js
+++ src/_Component/patent_map.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import VisibilitySensor from "react-visibility-sensor";
 
 import "../_Styles/patent_map_style.css";
 import logo1 from "../_Source/patent-logo-1.png";
@@ -9,50 +11,66 @@ class PatentMap extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = { visibility: false };
   }
 
   render() {
+    const { visibility } = this.state;
     return (
       <div className="patent_map">
-        <div className="patent_map_main">
-          <h1 className="patent_map_title">US Patent </h1>
-          <div className="patent_map_info">
-            <p>
-              The product is resistant to tears, soft chemicals,
-              puncture damage and ultraviolet light exposure.
-              It’s also unaffected by hydrocarbons, mildew, rot and the freeze and thaw cycle.
-            </p>
-            <ul className="patent_list">
-              <li>
-                <img className="patent_1" src={logo1} alt="logo1" width="53" />
-                <p className="patent_desc">
-                  Industrial Fabrics
-                  <br />
-                  {" "}
-                  International Award
+        <VisibilitySensor
+          partialVisibility="top"
+          offset={{ top: 800 }}
+          onChange={(isVisible) => {
+            this.setState({ visibility: isVisible });
+          }}
+        >
+          <div className="patent_map_main">
+            <Fade bottom when={!visibility} distance="50%">
+              <h1 className="patent_map_title">US Patent </h1>
+            </Fade>
+            <div className="patent_map_info">
+              <Fade bottom when={!visibility} distance="50%">
+                <p className="patent_map_p">
+                  The product is resistant to tears, soft chemicals,
+                  puncture damage and ultraviolet light exposure.
+                  It’s also unaffected by hydrocarbons, mildew, rot and the freeze and thaw cycle.
                 </p>
-              </li>
-              <li>
-                <img className="patent_1" src={logo2} alt="logo2" width="53" />
-                <p className="patent_desc">
-                  Top 3 of 100
-                  <br />
-                  {" "}
-                  suppliers by CSX
-                </p>
-              </li>
-              <li>
-                <img className="patent_1" src={logo3} alt="logo3" width="53" />
-                <p className="patent_desc">
-                  3 Safety Awards
-                  <br />
-                  {" "}
-                  from Advocare
-                </p>
-              </li>
-            </ul>
+              </Fade>
+              <Fade bottom cascade when={!visibility} distance="40%" duration={500}>
+                <ul className="patent_list">
+                  <li>
+                    <img className="patent_1" src={logo1} alt="logo1" width="53" />
+                    <p className="patent_desc">
+                      Industrial Fabrics
+                      <br />
+                      {" "}
+                      International Award
+                    </p>
+                  </li>
+                  <li>
+                    <img className="patent_1" src={logo2} alt="logo2" width="53" />
+                    <p className="patent_desc">
+                      Top 3 of 100
+                      <br />
+                      {" "}
+                      suppliers by CSX
+                    </p>
+                  </li>
+                  <li>
+                    <img className="patent_1" src={logo3} alt="logo3" width="53" />
+                    <p className="patent_desc">
+                      3 Safety Awards
+                      <br />
+                      {" "}
+                      from Advocare
+                    </p>
+                  </li>
+                </ul>
+              </Fade>
+            </div>
           </div>
-        </div>
+        </VisibilitySensor>
       </div>
     );
   }

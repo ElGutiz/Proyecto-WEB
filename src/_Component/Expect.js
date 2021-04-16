60555c8c7397d58e168aee43c0ab6d2f525174da
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-04-16 04:13:29 -0600

    Animation added

--- src/_Component/Expect.js
+++ src/_Component/Expect.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import VisibilitySensor from "react-visibility-sensor";
 
 import "../_Styles/expect_style.css";
 import workers from "../_Source/workers.jpg";
@@ -7,20 +9,32 @@ class Expect extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = { visibility: false };
   }
 
   render() {
+    const { visibility } = this.state;
     return (
-      <div className="Main6">
-        <div className="Mainn6">
-          <h2 className="Title_6">What to expect? </h2>
-          <p className="info_6">From design to installation, Rollpark can handle every aspect of the job: site procurement, civil engineering, geotechnical engineering, design, site preparation, and installation.</p>
-          <button button type="button" className="Installation_process_button">
-            <p className="Installation_process_button_text">Installation Process </p>
-          </button>
+      <VisibilitySensor
+        partialVisibility="top"
+        offset={{ top: 800 }}
+        onChange={(isVisible) => {
+          this.setState({ visibility: isVisible });
+        }}
+      >
+        <div className="Main6">
+          <Fade bottom when={!visibility} distance="30%" cascade>
+            <div className="Mainn6">
+              <h2 className="Title_6">What to expect? </h2>
+              <p className="info_6">From design to installation, Rollpark can handle every aspect of the job: site procurement, civil engineering, geotechnical engineering, design, site preparation, and installation.</p>
+              <button button type="button" className="Installation_process_button">
+                <p className="Installation_process_button_text">Installation Process </p>
+              </button>
+            </div>
+          </Fade>
+          <img className="worker_image" src={workers} alt="worker" width="300" />
         </div>
-        <img className="worker_image" src={workers} alt="worker" width="300" />
-      </div>
+      </VisibilitySensor>
     );
   }
 }

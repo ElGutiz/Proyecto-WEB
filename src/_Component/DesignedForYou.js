5d09fb2c8bf6f17f946355d88242a9311da125b7
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-04-16 02:00:36 -0600

    DesignedForYou Animation Finished and Style changed

--- src/_Component/DesignedForYou.js
+++ src/_Component/DesignedForYou.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import "../_Styles/DesignedForYou_style.css";
+import Fade from "react-reveal/Fade";
+import VisibilitySensor from "react-visibility-sensor";
 import parkImage from "../_Source/designed.png";
 import parkShadow from "../_Source/designedShadow.png";
 
@@ -8,27 +10,43 @@ class DesignedForYou extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = { visibility: false };
   }
 
   render() {
+    const { visibility } = this.state;
     return (
       <div className="Main">
-        <div className="Mainn2">
-          <div className="main_title">
-            <h2>Designed for You.</h2>
+        <VisibilitySensor
+          partialVisibility="top"
+          offset={{ top: 800 }}
+          onChange={(isVisible) => {
+            this.setState({ visibility: isVisible });
+          }}
+        >
+          <div className="Mainn2" style={{ opacity: visibility ? 0 : 1 }}>
+            <Fade bottom when={!visibility} distance="50%">
+              <div className="main_title">
+                <h2>Designed for You.</h2>
+              </div>
+            </Fade>
+            <div className="Facts">
+              <Fade bottom cascade when={!visibility}>
+                <ul className="Facts_list">
+                  <li> Roll it out and instantly park</li>
+                  <li> Re-use it on multiple sites</li>
+                  <li> Event Parking, Overflow, Vehicle Storage</li>
+                  <li className="m1"> Daily, temporary use </li>
+                  <li className="m2"> Daily, long term use </li>
+                </ul>
+              </Fade>
+            </div>
+            <Fade top when={!visibility} distance="20%">
+              <img className="shadow" src={parkShadow} alt="sombra" />
+              <img className="mainImage" src={parkImage} alt="parqueo" />
+            </Fade>
           </div>
-          <div className="Facts">
-            <ul className="Facts_list">
-              <li> Roll it out and instantly park</li>
-              <li> Re-use it on multiple sites</li>
-              <li> Event Parking, Overflow, Vehicle Storage</li>
-              <li className="m1"> Daily, temporary use </li>
-              <li className="m2"> Daily, long term use </li>
-            </ul>
-          </div>
-          <img className="shadow" src={parkShadow} alt="sombra" />
-          <img className="mainImage" src={parkImage} alt="parqueo" />
-        </div>
+        </VisibilitySensor>
       </div>
     );
   }

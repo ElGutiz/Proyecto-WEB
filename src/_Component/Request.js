8b4cdd5e2717fbfe4fa6bb07eca13bc24e91cd88
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-04-16 19:35:04 -0600

    Animation added

--- src/_Component/Request.js
+++ src/_Component/Request.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import VisibilitySensor from "react-visibility-sensor";
 
 import "../_Styles/Request_style.css";
 
@@ -6,23 +8,35 @@ class Request extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = { visibility: false };
   }
 
   render() {
+    const { visibility } = this.state;
     return (
       <div className="Main12">
-        <div className="Mainn12">
-          <h1 className="Mainn12_title">REQUEST A PROPOSAL</h1>
-          <p className="Mainn12_description">
-            Leave your details for our manager to contact you.
-            <br />
-            Let our estimating and design team assess your project.
-            <br />
-            Receive a proposal and free consultation.
-          </p>
-          <button type="button" className="Mainn12_button">Request a Quote</button>
-          <p className="Mainn12_description_bottom">Save on your next parking project with Rollpark, the most advanced and eco-friendly semi-permanent parking solution to date.</p>
-        </div>
+        <VisibilitySensor
+          partialVisibility="top"
+          offset={{ top: 800 }}
+          onChange={(isVisible) => {
+            this.setState({ visibility: isVisible });
+          }}
+        >
+          <div className="Mainn12">
+            <Fade bottom when={!visibility} distance="50%">
+              <h1 className="Mainn12_title">REQUEST A PROPOSAL</h1>
+              <p className="Mainn12_description">
+                Leave your details for our manager to contact you.
+                <br />
+                Let our estimating and design team assess your project.
+                <br />
+                Receive a proposal and free consultation.
+              </p>
+              <button type="button" className="Mainn12_button">Request a Quote</button>
+              <p className="Mainn12_description_bottom">Save on your next parking project with Rollpark, the most advanced and eco-friendly semi-permanent parking solution to date.</p>
+            </Fade>
+          </div>
+        </VisibilitySensor>
       </div>
     );
   }
